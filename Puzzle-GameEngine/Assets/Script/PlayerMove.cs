e6610488b1f1e0a315f8893d37c14c999a88713f
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-03-11 09:35:18 -0300

    Quase acabando a Cena1

--- Puzzle-GameEngine/Assets/Script/PlayerMove.cs
+++ Puzzle-GameEngine/Assets/Script/PlayerMove.cs
@@ -6,34 +6,35 @@ using UnityEngine;
 public class PlayerMove : MonoBehaviour
 {
     Rigidbody2D rb;
-    float horizontal;
     [SerializeField] float speedX = 5;
     [SerializeField] float jumpForce = 200;
-  //  [SerializeField] float swimsForce = 250;
+    [SerializeField] float swimsForce = 250;
+    float horizontal;
 
+    // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
-
     }
 
     // Update is called once per frame
     void Update()
     {
         horizontal = Input.GetAxisRaw("Horizontal");
-       // if (Input.GetButtonDown("Fire3"))
-       // {
-        //    rb.AddForce(new Vector2(0, -swimsForce));
-       // }
+        if (Input.GetButtonDown("Fire3"))
+        {
+            rb.AddForce(new Vector2(0, -swimsForce));
+        }
         if (Input.GetButtonDown("Jump"))
         {
             rb.AddForce(new Vector2(0, jumpForce));
         }
-
     }
-    private void FixedUpdate()
+    private void FixedUpdate()// Para o uso da  Fisica
     {
-        if (horizontal != 0)
-            rb.velocity = new Vector2(horizontal * speedX, rb.velocity.y);
+        rb.AddForce(new Vector2(horizontal * speedX,  0), ForceMode2D.Force);
     }
+   
+
+
 }
