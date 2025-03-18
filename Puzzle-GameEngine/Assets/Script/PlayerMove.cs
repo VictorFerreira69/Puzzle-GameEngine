3b30f159072fcbb82b7f2c98e135444f4142dc6f
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-03-18 16:35:44 -0300

    Impossivel de funcionar

--- Puzzle-GameEngine/Assets/Script/PlayerMove.cs
+++ Puzzle-GameEngine/Assets/Script/PlayerMove.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
@@ -10,9 +11,11 @@ public class PlayerMove : MonoBehaviour
 
     [SerializeField] float speedX = 5;
     [SerializeField] float jumpForce = 200;
+    [SerializeField] float addForceSpeed = 10;  // Velocidade para o modo AddForce
     private float horizontal;
     private bool isGrounded = true;
     private float groundCheckRadius = 0.2f;
+    private bool useAddForce = false;  // Flag para determinar o modo de movimentação
     public Transform groundCheck;
     public LayerMask groundLayer;
 
@@ -26,12 +29,11 @@ public class PlayerMove : MonoBehaviour
     {
         horizontal = Input.GetAxisRaw("Horizontal");
 
-        
         float speed = Mathf.Abs(horizontal);
         if (speed < 0.01f) speed = 0;
         animator.SetFloat("Speed", speed);
 
-       
+        // Flip do personagem
         if (horizontal < 0 && transform.localScale.x > 0)
         {
             transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
@@ -41,42 +43,60 @@ public class PlayerMove : MonoBehaviour
             transform.localScale = new Vector2(transform.localScale.x * -1, transform.localScale.y);
         }
 
-       
+        // Pulo
         if (Input.GetButtonDown("Jump") && isGrounded)
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpForce);
             animator.SetBool("IsJumping", true);
         }
-
-       
-        
     }
 
     void FixedUpdate()
     {
-        
-        if (Mathf.Abs(horizontal) > 0.01f)
+        if (useAddForce)
         {
-            rb.AddForce(new Vector2(horizontal * speedX, 0), ForceMode2D.Impulse);
+            MoveWithAddForce();
+        }
+        else
+        {
+            MoveNormally();
         }
 
-        
+        // Verificação de chão
         isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
 
-       
+        // Atualiza a animação de pulo
         if (rb.velocity.y == 0 && !isGrounded)
         {
-            animator.SetBool("IsJumping", false); 
+            animator.SetBool("IsJumping", false);
         }
     }
 
-   
+    void MoveNormally()
+    {
+        if (Mathf.Abs(horizontal) > 0.01f)
+        {
+            rb.velocity = new Vector2(horizontal * speedX, rb.velocity.y);
+        }
+    }
+
+    void MoveWithAddForce()
+    {
+        rb.AddForce(new Vector2(horizontal * addForceSpeed, 0), ForceMode2D.Force);
+    }
+
     private void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = true;
-            animator.SetBool("IsJumping", false); 
+            animator.SetBool("IsJumping", false);
+        }
+
+        // Ativa o modo AddForce ao colidir com o Surface Effector
+        if (collision.gameObject.CompareTag("SurfaceEffector"))
+        {
+            useAddForce = true;
         }
     }
 
@@ -85,7 +105,13 @@ public class PlayerMove : MonoBehaviour
         if (collision.gameObject.CompareTag("Ground"))
         {
             isGrounded = false;
-            animator.SetBool("IsJumping", true); 
+            animator.SetBool("IsJumping", true);
+        }
+
+        // Desativa o modo AddForce ao sair do Surface Effector
+        if (collision.gameObject.CompareTag("SurfaceEffector"))
+        {
+            useAddForce = false;
         }
     }
 }
