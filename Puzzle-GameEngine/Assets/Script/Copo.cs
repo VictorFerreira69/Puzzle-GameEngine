42475c54b0cd798783eb5473f5cf5ad57f630e31
VictorFerreira69 <victor13.09.08ferreira@gmail.com>
2025-03-19 09:13:15 -0300

    Mudando cenario Fase3

--- Puzzle-GameEngine/Assets/Script/Copo.cs
+++ Puzzle-GameEngine/Assets/Script/Copo.cs
@@ -5,8 +5,7 @@ using UnityEngine;
 public class Copo : MonoBehaviour
 {
     public float targetWeight = 42f;
-    public SpriteRenderer medidorSpriteRenderer;
-    public Sprite[] medidorSprites;
+   
     private float currentWeight;
 
     public Sprite copoNormalSprite;
@@ -20,14 +19,7 @@ public class Copo : MonoBehaviour
 
     void Update()
     {
-        if (isBoxNear)
-        {
-            medidorSpriteRenderer.sprite = copoLevantadoSprite;
-        }
-        else
-        {
-            medidorSpriteRenderer.sprite = copoNormalSprite;
-        }
+       
     }
 
     void OnTriggerEnter2D(Collider2D other)
@@ -60,8 +52,7 @@ public class Copo : MonoBehaviour
 
     void UpdateWeightUI()
     {
-        int spriteIndex = Mathf.Clamp((int)((currentWeight / targetWeight) * (medidorSprites.Length - 1)), 0, medidorSprites.Length - 1);
-        medidorSpriteRenderer.sprite = medidorSprites[spriteIndex];
+       
 
         if (currentWeight >= targetWeight)
         {
